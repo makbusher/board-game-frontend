2ec4f4ca0295851d72df7b4aae660a33c7783ad4
Makayla <makaylabusher@gmail.com>
2023-02-06 15:22:58 -0600

    logout link

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,11 @@
 import { Signup } from "./Signup";
+import { LogoutLink } from "./LogoutLink";
 
 export function Content() {
   return (
     <div>
       <Signup />
+      <LogoutLink />
     </div>
   );
 }
