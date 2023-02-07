10eb40fd38b97948ce64bb8f31cabe5c98ad850a
Makayla <makaylabusher@gmail.com>
2023-02-07 14:55:08 -0600

    add favorite button to games show

--- src/GamesShow.jsx
+++ src/GamesShow.jsx
@@ -1,10 +1,25 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function GamesShow(props) {
+  const [favorites, setFavorites] = useState([]);
+
+  const handleCreateFavorite = (gameId, successCallback) => {
+    console.log("handlingCreateFavorite");
+    axios.post(`http://localhost:3000/games/${gameId}/favorites.json`).then((response) => {
+      setFavorites([...favorites, response.data]);
+      successCallback;
+    });
+  };
+
   return (
     <div>
       <h1>About {props.game.name}</h1>
+      <p>{props.game.id}</p>
       <p>Description: {props.game.description}</p>
       <p>Players: {props.game.players}</p>
       <p>Category: {props.game.category}</p>
+      <button onClick={() => handleCreateFavorite(props.game.id)}>Add to Favorites</button>
     </div>
-  )
+  );
 }
