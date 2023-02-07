d99512908a35b5d30fec148a48c2f19f3b90e6ce
Makayla <makaylabusher@gmail.com>
2023-02-07 12:22:44 -0600

    add games index

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,21 @@
 import { Signup } from "./Signup";
 import { LogoutLink } from "./LogoutLink";
 import { FavoritesIndex } from "./FavoritesIndex";
+import { GamesIndex } from "./GamesIndex";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export function Content() {
   const [favorites, setFavorites] = useState([]);
+  const [games, setGames] = useState([]);
+
+  const handleIndexGames = () => {
+    console.log("handleIndexGames");
+    axios.get("http://localhost:3000/games.json").then((response) => {
+      console.log(response.data);
+      setGames(response.data);
+    });
+  };
   
   const handleIndexFavorites = () => {
     console.log("handleIndexFavorites");
@@ -16,11 +26,13 @@ export function Content() {
   };
 
   useEffect(handleIndexFavorites, []);
+  useEffect(handleIndexGames, []);
 
   return (
     <div>
       <Signup />
       <LogoutLink />
+      <GamesIndex games={games} />
       <FavoritesIndex favorites={favorites} />
     </div>
   );
---
+++ src/GamesIndex.jsx
@@ -0,0 +1,13 @@
+export function GamesIndex(props) {
+  return (
+    <div id="games-index">
+      <h1>Board Games</h1>
+      {props.games.map((game) => (
+        <div key={game.id}>
+          <h2>{game.name}</h2>
+          <img src={game.image_url} />
+        </div>
+      ))}
+    </div>
+  );
+}
