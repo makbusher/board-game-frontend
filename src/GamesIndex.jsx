7833346a2a62e4c9c061dd83089b384e9b7547b0
Makayla <makaylabusher@gmail.com>
2023-02-08 14:27:57 -0600

    sample card layout for games index

--- src/GamesIndex.jsx
+++ src/GamesIndex.jsx
@@ -5,13 +5,22 @@ export function GamesIndex(props) {
     <div id="games-index">
       <h1>Board Games</h1>
       {props.games.map((game) => (
-        <div key={game.id}>
-          <Link to={`/games/${game.id}`}>
-            <h2>{game.name}</h2>
-          </Link>
-          <img src={game.image_url} />
+        <div className="games" key={game.id}>
+          <div className="card" style={{width: '19rem'}}>
+            <img src={game.image_url} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <div className="card-text">
+                <Link to={`/games/${game.id}`}>
+                  <center>                        
+                    {game.name}
+                  </center>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
   );
 }
+
