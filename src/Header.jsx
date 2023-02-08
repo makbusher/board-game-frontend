593faacd971acec5bfea90238e6814162ab9ed1f
Makayla <makaylabusher@gmail.com>
2023-02-08 11:45:30 -0600

    add navbar + install bootstrap

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
-    <header>
-      <nav>
-        <a href="/">Home</a> | <a href="/favorites">My Favorites</a> | <a href="/signup">Signup</a>
-      </nav>
-    </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <Link className="nav-link" to="/favorites">My Favorites</Link>
+            <Link className="nav-link" to="/signup">Signup</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
+
