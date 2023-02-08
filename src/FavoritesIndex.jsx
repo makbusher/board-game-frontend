d6a2a8845e4241a08b414ba5112eda53195fbd42
Makayla <makaylabusher@gmail.com>
2023-02-08 15:11:00 -0600

    add search filter to favorites

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export function FavoritesIndex (props) {
   const [favorites, setFavorites] = useState([]);
+  const [searchFilter, setSearchFilter] = useState("");
 
   const handleDestroyFavorite = (favorite) => {
     console.log("handleDestroyFavorite", favorite);
@@ -14,7 +15,13 @@ export function FavoritesIndex (props) {
   return (
     <div id="favorites-index">
       <h1>My Favorites</h1>
-      {props.favorites.map((favorite) => (
+      Search: <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} list="names" />
+      <datalist id="names">
+        {props.favorites.map(favorite => (
+          <option key={favorite.id} value={favorite.game.name} />
+        ))}
+      </datalist>
+      {props.favorites.filter((favorite) => favorite.game.name.toLowerCase().includes(searchFilter.toLowerCase())).map((favorite) => (
         <div key={favorite.id}>
           <h2>{favorite.game.name}</h2>            
           <img src={favorite.game.image_url} />
