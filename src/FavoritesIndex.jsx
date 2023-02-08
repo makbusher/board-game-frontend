2562483f31c51927343747a1966ba29dd8045b07
Makayla <makaylabusher@gmail.com>
2023-02-08 13:02:13 -0600

    fix favorites index

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -6,8 +6,8 @@ export function FavoritesIndex (props) {
 
   const handleDestroyFavorite = (favorite) => {
     console.log("handleDestroyFavorite", favorite);
-    axios.delete(`http://localhost:3000/favorites/${favorite.id}.json`).then((response) => {
-      setFavorites(favorites.filter((p) => p.id !== favorite.id));
+    axios.delete(`http://localhost:3000/favorites/${favorite.favorite_id}.json`).then((response) => {
+      setFavorites(favorites.filter((f) => f.id !== favorite.favorite_id));
     });
   };
 
@@ -16,8 +16,8 @@ export function FavoritesIndex (props) {
       <h1>My Favorites</h1>
       {props.favorites.map((favorite) => (
         <div key={favorite.id}>
-          <h2>{favorite.name}</h2>
-          <img src={favorite.image_url} />
+          <h2>{favorite.game.name}</h2>
+          <img src={favorite.game.image_url} />
           <button onClick={() => handleDestroyFavorite(favorite)}>Remove from Favorites</button>
         </div>
       ))}
