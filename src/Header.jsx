775de56fd06c7c2c5e103d5b2d468745cde4fe61
Makayla <makaylabusher@gmail.com>
2023-02-09 11:13:59 -0600

    change header/header text color

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Board Game Review</a>
+        <a className="navbar-brand navbar-dark bg-secondary" href="#">Board Game Review</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
+          <div className="navbar-nav navbar-dark bg-dark">
             <a className="nav-link active" aria-current="page" href="/">Home</a>
             <Link className="nav-link" to="/favorites">My Favorites</Link>
             <Link className="nav-link" to="/signup">Signup</Link>
