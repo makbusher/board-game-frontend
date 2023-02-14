e77f000ef719178b920ca7e3a9ac2547c95081ca
Makayla <makaylabusher@gmail.com>
2023-02-14 14:14:28 -0600

    change header/footer design

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 import { Login } from "./Login";
-import logo from './images/BGR-logos_black.png';
+import logo from './images/BGR-logos.jpeg';
 
 export function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-secondary" data-bs-theme="light">
+    <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
       <div className="container-fluid">
         <Link to="/" className="nav-link"><img src={logo} width="100em" height="60em" /></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
